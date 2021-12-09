4dbbfd0ba7c3fd0f25f7b4ec5cb616170172d6df
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2021-12-09 13:08:34 -0800

    v2.2 version bump

--- vdbCreate.swift
+++ vdbCreate.swift
@@ -3,14 +3,14 @@
 //  VDBCreate
 //
 //  Copyright (c) 2021  Anthony West, Caltech
-//  Last modified 8/20/21
+//  Last modified 12/9/21
 
 import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
 
-let version : String = "2.1"
+let version : String = "2.2"
 let checkForVDBUpdate : Bool = true
 
 print("SARS-CoV-2 Variant Database Creator  Version \(version)      Bjorkman Lab/Caltech")
