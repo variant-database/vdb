66650fbbe2cd06329e76fe28465be50624360456
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2022-03-24 14:51:45 -0700

    v2.5 version bump

--- vdbCreate.swift
+++ vdbCreate.swift
@@ -3,14 +3,14 @@
 //  VDBCreate
 //
 //  Copyright (c) 2022  Anthony West, Caltech
-//  Last modified 2/28/22
+//  Last modified 3/24/22
 
 import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
 
-let version : String = "2.4"
+let version : String = "2.5"
 let checkForVDBUpdate : Bool = true
 let mpNumberDefault : Int = 12
 let basePath : String = FileManager.default.currentDirectoryPath
